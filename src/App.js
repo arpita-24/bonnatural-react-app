969841700d6de382d967d95f178b589d9fd8fc68
Arpita <marpita721@gmail.com>
2022-07-29 02:23:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
     return (
         <div>
+
             <h1>WELCOME LORD!</h1>
+            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM  from 'react';
 
 import App from './App';
 
+
+
 ReactDOM.render(<App />,document.getElementById('root'));
